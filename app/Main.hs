15738bc083f613a6c710eac1ce8ad6aa6ed89807
will <will@yager.io>
2020-03-07 20:45:06 +0800

    exn

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
 import Lib
-import System.Exit (exitFailure)
-import Control.Exception (onException)
 
 main :: IO ()
-main = runCommand `onException` exitFailure
+main = runCommand
